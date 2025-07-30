85224e27f1af818ea5fb127f63bf6e993a32ec7e
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-07-30 17:16:15 +0200

    add position to tables

--- restaurant-simulator/src/main/java/env/model/Table.java
+++ restaurant-simulator/src/main/java/env/model/Table.java
@@ -3,10 +3,12 @@ package env.model;
 public class Table {
     private final TableId id;
     private boolean free;
+    private Position2D position;
 
     public Table(TableId id) {
         this.id = id;
         this.free = true;
+        this.position = new Position2D(0, 0);
     }
 
     public TableId getId() {
@@ -19,5 +21,13 @@ public class Table {
 
     public void setFree(boolean free) {
         this.free = free;
+    }
+
+    public Position2D getPosition() {
+        return position;
+    }
+    
+    public void setPosition(Position2D position) {
+        this.position = position;
     }
 }
