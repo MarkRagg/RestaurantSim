c55bd16710b0fa416f10398bf9d28a7b9cf2ddf2
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-07-30 17:16:50 +0200

    add and implement methods for the view

--- restaurant-simulator/src/main/java/env/interfaces/Restaurant.java
+++ restaurant-simulator/src/main/java/env/interfaces/Restaurant.java
@@ -1,12 +1,18 @@
 package env.interfaces;
 
 import java.util.List;
+import java.util.Set;
 
 import env.model.CustomerId;
+import env.model.Position2D;
 import env.model.Table;
 import env.model.TableId;
 
 public interface Restaurant {
+
+  int getWidth();
+
+  int getHeight();
 
   /**
    * Returns the list of tables in the restaurant.
@@ -29,6 +35,18 @@ public interface Restaurant {
   Table getTable(TableId id);
 
   /**
+   * Returns the set of all agents in the restaurant.
+   * @return a set of agent names
+   */
+  Set<String> getAllAgents();
+
+  /**
+   * 
+   * @param customerId
+   */
+  Position2D getAgentPosition(String agentId);
+
+  /**
    * Adds a customer to the queue.
    * @param customerId
    */
@@ -45,4 +63,30 @@ public interface Restaurant {
    * @param customerId
    */
   CustomerId getNextInQueue();
+
+  /**
+   * Returns true if the restaurant contains an agent with the specified name.
+   * @param agentName
+   * @return true if the agent exists, false otherwise
+   */
+  boolean containsAgent(String agentName);
+
+  /**
+   * Returns the agent with the specified name.
+   * @param name
+   * @return the agent
+   */
+  void addAgent(String name);
+
+  boolean removeAgent(String name);
+
+  Set<String> getRemovedAgents();
+
+  boolean isAgentRemoved(String agentName);
+
+  boolean setAgentLocationToTable(String agent, TableId tableId);
+
+  boolean setAgentLocationToChef(String agent, String chefName);
+  
+  boolean setAgentLocationToQueue(String agent);
 }
--- restaurant-simulator/src/main/java/env/model/RestaurantImpl.java
+++ restaurant-simulator/src/main/java/env/model/RestaurantImpl.java
@@ -1,17 +1,39 @@
 package env.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.IntStream;
 
 import env.interfaces.Restaurant;
+import java.util.HashSet;
 
 public class RestaurantImpl implements Restaurant {
     private final List<Table> tables;
     private final List<CustomerId> queue;
+    private final Map<String, Agent> agents;
+    private final Set<String> removedAgents;
+    private final RestaurantSize restaurantSize;
 
-    public RestaurantImpl(List<Table> tables) {
+    public RestaurantImpl(List<Table> tables, RestaurantSize restaurantSize) {
+        this.restaurantSize = restaurantSize;
+        this.removedAgents = new HashSet<>();
         this.tables = tables;
+        this.calculateTablePositions();
         this.queue = new ArrayList<>();
+        this.agents = new HashMap<>();
+    }
+
+    @Override
+    public int getWidth() {
+        return this.restaurantSize.getWidth();
+    }
+
+    @Override
+    public int getHeight() {
+        return this.restaurantSize.getHeight();
     }
 
     @Override
@@ -36,6 +58,20 @@ public class RestaurantImpl implements Restaurant {
     }
 
     @Override
+    public Set<String> getAllAgents() {
+        synchronized (this.agents) {
+            return this.agents.keySet();
+        }
+    }
+
+    @Override
+    public Position2D getAgentPosition(String agentId) {
+        synchronized ( this.agents ) {
+            return this.agents.get(agentId).getPosition();
+        }
+    }
+
+    @Override
     public List<CustomerId> getQueue() {
         return this.queue;
     }
@@ -50,5 +86,157 @@ public class RestaurantImpl implements Restaurant {
         CustomerId customerId = this.queue.get(0);
         this.queue.remove(0); 
         return customerId;
+    }
+
+    @Override
+    public boolean containsAgent(String agentName) {
+        synchronized (this.agents) {
+            return this.agents.containsKey(agentName);
+        }
+    }
+
+    @Override
+    public void addAgent(String name) {
+        synchronized (this.agents) {
+            this.agents.put(name, new Agent(name, this.calculateAgentPosition(name)));
+        }
+    }
+
+    @Override
+    public boolean removeAgent(String name) {
+        synchronized (this) {
+            if (this.agents.containsKey(name)) {
+                this.agents.remove(name);
+                this.removedAgents.add(name);
+                return true;
+            }       
+            return false;
+        }
+    }
+
+    @Override
+    public Set<String> getRemovedAgents() {
+        synchronized (this.removedAgents) {
+            return new HashSet<>(this.removedAgents);
+        }
+    }
+
+    @Override
+    public boolean isAgentRemoved(String agentName) {
+        synchronized (this.removedAgents) {
+            return this.removedAgents.contains(agentName);
+        }
+    }
+
+    @Override
+    public boolean setAgentLocationToTable(String agentName, TableId tableId) {
+        synchronized (this.agents) {
+            Agent agent = this.agents.get(agentName);
+            Position2D tablePosition = getTable(tableId).getPosition();
+            Position2D newPosition = this.calculateAgentPositionToTargetPosition(agentName, tablePosition);
+            if (this.checkValidMovement(newPosition)) {
+                agent.setPosition(newPosition);
+                return true;
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public boolean setAgentLocationToChef(String agentName, String chefName) {
+        synchronized (this.agents) {
+            Agent agent = this.agents.get(agentName);
+            Position2D chefPosition = this.agents.get(chefName).getPosition();
+            Position2D newPosition = this.calculateAgentPositionToTargetPosition(agentName, chefPosition);
+            if (this.checkValidMovement(newPosition)) {
+                agent.setPosition(newPosition);
+                return true;
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public boolean setAgentLocationToQueue(String agentName) {
+        synchronized (this.agents) {
+            Agent agent = this.agents.get(agentName);
+            Position2D newPosition = this.calculateAgentToQueuePosition(agent.getPosition());
+            if (this.checkValidMovement(newPosition)) {
+                agent.setPosition(newPosition);
+                return true;
+            }
+            return false;
+        }
+    }
+
+    private boolean checkValidMovement(Position2D targetPosition) {
+        return targetPosition.getX() < this.restaurantSize.getWidth() &&
+               targetPosition.getY() < this.restaurantSize.getHeight() && 
+               targetPosition.getX() >= 0 &&
+               targetPosition.getY() >= 0; 
+    }
+
+    private void calculateTablePositions() {
+        int halfHeight = this.restaurantSize.getHeight() / 2;
+        int spaceFromEdge = 2;
+        int tablesCount = this.tables.size();
+        int spaceBetweenTables = (this.restaurantSize.getWidth() - (2 * spaceFromEdge)) / (tablesCount - 1);
+        for (int i = 0; i < tablesCount; i++) {
+            int x = spaceFromEdge + (i * spaceBetweenTables);
+            int y = halfHeight;
+            Table table = this.tables.get(i);
+            table.setPosition(new Position2D(x, y));
+        }
+    }
+
+    // private Position2D calculateWaiterPositions() {
+    //     int height = this.restaurantSize.getHeight() * 7 / 10;
+    //     return IntStream.range(0, getWidth() - 1)
+    //         .mapToObj(i -> new Position2D(i, height))
+    //         .filter(pos -> this.agents.values().stream()
+    //             .noneMatch(agent -> agent.getPosition().equals(pos)))
+    //         .findFirst()
+    //         .orElse(new Position2D(0, height));
+    // }
+
+    private Position2D calculateAgentToQueuePosition(Position2D agentPosition) {
+        return new Position2D(0, 0);
+    }
+
+    private Position2D calculateAgentPositionToTargetPosition(String agentName, Position2D targetPosition) {
+        return agentName.startsWith("waiter") ? 
+            new Position2D(targetPosition.getX(), targetPosition.getY() - 1) : 
+            new Position2D(targetPosition.getX() - 1, targetPosition.getY());
+
+    }
+
+    private Position2D calculateChefPosition() {
+    return IntStream.range(1, getWidth() - 1)
+        .filter(i -> i % 2 == 1)
+        .mapToObj(i -> new Position2D(i, restaurantSize.getHeight() - 1))
+        .filter(pos -> this.agents.values().stream()
+            .noneMatch(agent -> agent.getPosition().equals(pos)))
+        .findFirst()
+        .orElse(new Position2D(0, restaurantSize.getHeight() - 1));
+    } 
+
+    private Position2D calculateCustomerPosition() {
+        return IntStream.range(0, getWidth() - 1)
+            .mapToObj(i -> new Position2D(i, 0))
+            .filter(pos -> this.agents.values().stream()
+                .noneMatch(agent -> agent.getPosition().equals(pos)))
+            .findFirst()
+            .orElse(new Position2D(0, 0));
+    }
+
+    private Position2D calculateAgentPosition(String agentName) {
+        if (agentName.startsWith("waiter")) {
+            return new Position2D(restaurantSize.getWidth() - 1, restaurantSize.getHeight() - 1);
+        } else if (agentName.startsWith("chef")) {
+            return this.calculateChefPosition(); 
+        } else if (agentName.startsWith("customer")) {
+            return calculateCustomerPosition(); 
+        }
+        return new Position2D(0, 0); 
     }
 }
