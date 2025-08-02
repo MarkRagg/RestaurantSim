42c3091affc48a023704c3561e9eafa1db1560d7
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-08-02 11:45:16 +0200

    add more tests

--- restaurant-simulator/src/test/java/model/RestaurantImplTest.java
+++ restaurant-simulator/src/test/java/model/RestaurantImplTest.java
@@ -1,5 +1,6 @@
 package model;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -10,6 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import env.interfaces.Restaurant;
+import env.model.CustomerId;
 import env.model.Position2D;
 import env.model.RestaurantImpl;
 import env.model.RestaurantSize;
@@ -95,9 +97,64 @@ public class RestaurantImplTest {
     this.restaurant.addAgent(chef1);
     this.restaurant.addAgent(chef2);
     this.restaurant.addAgent(chef3);
-    assertEquals(this.restaurant.getAgentPosition(chef1), Position2D.of(x + 1, height - 1));
-    assertEquals(this.restaurant.getAgentPosition(chef2), Position2D.of(x + 3, height - 1));
-    assertEquals(this.restaurant.getAgentPosition(chef3), Position2D.of(x + 5, height - 1));
+    assertEquals(Position2D.of(x + 1, height - 1), this.restaurant.getAgentPosition(chef1));
+    assertEquals(Position2D.of(x + 3, height - 1), this.restaurant.getAgentPosition(chef2));
+    assertEquals(Position2D.of(x + 5, height - 1), this.restaurant.getAgentPosition(chef3));
+  }
+
+  @Test
+  public void testQueue() {
+    CustomerId customer1 = new CustomerId("customer_1");
+    CustomerId customer2 = new CustomerId("customer_2");
+    this.restaurant.addToQueue(customer1);
+    this.restaurant.addToQueue(customer2);
+    assertTrue(this.restaurant.getQueue().size() == 2);
+    this.restaurant.removeFromQueue(customer2);
+    assertTrue(this.restaurant.getQueue().size() == 1);
+    this.restaurant.removeFromQueue(customer1);
+    assertTrue(this.restaurant.getQueue().size() == 0);
+  }
+
+  @Test
+  public void testAgentLocationToQueue() {
+    int y = 0;
+    String customer1 = "customer_1";
+    String customer2 = "customer_2";
+    this.restaurant.addAgent(customer1);
+    this.restaurant.addAgent(customer2);
+    this.restaurant.addToQueue(new CustomerId(customer1));
+    this.restaurant.setAgentLocationToQueue(customer1);
+    assertEquals(Position2D.of(width - 1, y), this.restaurant.getAgentPosition(customer1));
+    this.restaurant.addToQueue(new CustomerId(customer2));
+    this.restaurant.setAgentLocationToQueue(customer2);
+    assertEquals(Position2D.of(width - 1, y), this.restaurant.getAgentPosition(customer2));
+    assertEquals(Position2D.of(width - 1, y + 1), this.restaurant.getAgentPosition(customer1));
+  }
+
+  @Test
+  public void testSetAgentLocationToChef() {
+    String waiter1 = "waiter_1";
+    String chef1 = "chef_1";
+    this.restaurant.addAgent(waiter1);
+    this.restaurant.addAgent(chef1);
+    this.restaurant.setAgentLocationToChef(waiter1, chef1);
+    Position2D chefPos = this.restaurant.getAgentPosition(chef1);
+    assertEquals(Position2D.of(chefPos.getX(), chefPos.getY() - 1), this.restaurant.getAgentPosition(waiter1));
+  }
+
+  @Test
+  public void testSetAgentLocationToDefault() {
+    String waiter1 = "waiter_1";
+    String waiter2 = "waiter_2";
+    this.restaurant.addAgent(waiter1);
+    this.restaurant.addAgent(waiter2);
+    this.restaurant.setAgentLocationToTable(waiter1, new TableId("table_1"));
+    this.restaurant.setAgentLocationToTable(waiter2, new TableId("table_2"));
+    this.restaurant.setAgentLocationToDefault(waiter2);
+    assertEquals(Position2D.of(width - 1, height - 1), this.restaurant.getAgentPosition(waiter2));
+    assertNotEquals(Position2D.of(width - 2, height - 1), this.restaurant.getAgentPosition(waiter1));
+    this.restaurant.setAgentLocationToDefault(waiter1);
+    assertEquals(Position2D.of(width - 2, height - 1), this.restaurant.getAgentPosition(waiter1));
   }
 
 }
